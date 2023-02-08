9409f34c776d726c5bcc2186a832db10948fbbc8
mn180342 <mn180342d@student.etf.bg.ac.rs>
2023-02-08 20:04:11 +0100

    Added profile fragment

--- app/src/main/java/com/example/pandica_anroid/ProfileFragment.java
+++ app/src/main/java/com/example/pandica_anroid/ProfileFragment.java
@@ -2,63 +2,103 @@ package com.example.pandica_anroid;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.pandica_anroid.Adapters.RecyclerAdapterPromoPackagesBuyingTickets;
+import com.example.pandica_anroid.Adapters.RecyclerAdapterSingleTicketsBuyingTickets;
+import com.example.pandica_anroid.Models.Packet;
+import com.example.pandica_anroid.Models.SinglePacket;
+import com.example.pandica_anroid.Models.User;
+
+
 public class ProfileFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //setting title
+        ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Pandica");
+
+        setHasOptionsMenu(true);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView username =  view.findViewById(R.id.profileUsernameValueTextViewProfile);
+        TextView firstName = view.findViewById(R.id.profileFirstNameValueTextViewProfile);
+        TextView lastName =  view.findViewById(R.id.profileLastNameValueTextViewProfile);
+        TextView phoneNumber = view.findViewById(R.id.profilePhoneNumberValueTextViewProfile);
+        TextView address =  view.findViewById(R.id.profileAddressValueTextViewProfile);
+
+        TextView buttonChange = view.findViewById(R.id.profileChangeButtonNotifications);
+
+        username.setText("  " + User.currentUser.getUsername());
+        firstName.setText("  " + User.currentUser.getFirstName());
+        lastName.setText("  " + User.currentUser.getLastName());
+        phoneNumber.setText("  " + User.currentUser.getPhoneNumber());
+        address.setText("  " + User.currentUser.getAddress());
+
+        int id = User.currentUser.getId();
+
+        buttonChange.setOnClickListener((view2) -> {
+            Navigation.findNavController(getView()).navigate(R.id.action_profileFragment_to_changeUserDetailsFragment);
+        });
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        menu.clear();
+        getActivity().getMenuInflater().inflate(R.menu.main_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+
+        switch (item.getItemId()){
+            case R.id.mainMenuBuyingTickets:
+                Navigation.findNavController(getView()).navigate(R.id.action_profileFragment_to_buyingTicketsFragment);
+                return true;
+            case R.id.mainMenuBoughtTickets:
+                Navigation.findNavController(getView()).navigate(R.id.action_profileFragment_to_boughtTicketsFragment);
+                return true;
+            case R.id.mainMenuEvents:
+                Navigation.findNavController(getView()).navigate(R.id.action_profileFragment_to_eventsFragment);
+                return true;
+            case R.id.mainMenuAnimals:
+                Navigation.findNavController(getView()).navigate(R.id.action_profileFragment_to_animalsFragment);
+                return true;
+            case R.id.mainMenuNotifications:
+                Navigation.findNavController(getView()).navigate(R.id.action_profileFragment_to_notificationsFragment);
+                return true;
+            case R.id.mainMenuProfile:
+                return true;
+            case R.id.mainMenulogout:
+                Navigation.findNavController(getView()).navigate(R.id.action_profileFragment_to_homeFragment);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
